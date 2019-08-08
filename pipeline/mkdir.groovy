04efd16de961cb66969220e1b2ac878d3cdb26f2
Alexey <aalterpesotskiy@zendesk.com>
2019-08-08 17:30:26 +0100

    test sh label

--- pipeline/mkdir.groovy
+++ pipeline/mkdir.groovy
@@ -1,7 +1,10 @@
 timeout(5) {
   node("master") {
     stage("Create folder") {
-      sh("mkdir -p ${FOLDER_NAME}")
+      sh(label: "testme label", script: """
+        pwd
+        mkdir -p ${FOLDER_NAME}
+      """)
       echo("${FOLDER_NAME} was successfully created.")
     }
   }
